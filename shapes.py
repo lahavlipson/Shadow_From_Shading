74bf719ce0dc73bf00f7146d5b76c88bc7b728b8
Niles Christensen <nnc2117@columbia.edu>
2019-02-23 16:53:45 -0500

    [Adds] a basic sphere class

--- shapes.py
+++ shapes.py
@@ -5,3 +5,12 @@ class Shape:
 
     def translate(self, offset):
         self.center = self.center + offset
+
+
+class Sphere(Shape):
+    def __init__(self, center, radius):
+        super().__init__(center)
+        self.radius = radius
+
+if __name__ == '__main__':
+    sp = Sphere((0, 0, 0), 5)
