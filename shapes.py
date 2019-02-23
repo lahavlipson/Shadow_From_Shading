1925bdb9188e17b8d601e1da0b59d5955ba60dc3
Niles Christensen <nnc2117@columbia.edu>
2019-02-23 16:59:02 -0500

    [Adds] the render method for Sphere

--- shapes.py
+++ shapes.py
@@ -1,3 +1,5 @@
+from renderer import Cir
+
 class Shape:
 
     def __init__(self, center):
@@ -17,5 +19,10 @@ class Sphere(Shape):
         super().__init__(center)
         self.radius = radius
 
+    def render(self):
+        # Returns a list for sake of consistency with other render methods
+        return [Cir((self.center, self.radius))]
+
 if __name__ == '__main__':
     sp = Sphere((0, 0, 0), 5)
+    shapes_so_far = sp.render()
