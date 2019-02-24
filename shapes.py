d05adeca43066bfe8e6ef8c2bdc5c0165ca44d2b
Niles Christensen <nnc2117@columbia.edu>
2019-02-24 17:55:09 -0500

    [Adds] a scale method and a cuboid class

--- shapes.py
+++ shapes.py
@@ -1,7 +1,6 @@
 from renderer import Cir
 
 class Shape:
-
     def __init__(self, center):
         self.center = center
 
@@ -14,14 +13,33 @@ class Shape:
     def render(self):
         pass
 
+    def scale(self, factor, axis=None):
+       pass
+
 class Sphere(Shape):
     def __init__(self, center, radius):
         super().__init__(center)
         self.radius = radius
 
+    def scale(self, factor, axis=None):
+        self.radius = radius * factor
+
     def render(self):
         # Returns a list for sake of consistency with other render methods
         return [Cir((self.center, self.radius))]
+
+class Cuboid(Shape):
+
+    def __init__(self, center, width, height, depth):
+        super().__init__(center)
+        self.side_lengths = np.array(width, height, depth)
+
+    def scale(self, factor, axis=None):
+        assert axis >= 0 and axis < 3
+        self.side_lengths[axis] = self.side_lengths[axis] * factor
+
+    def render(self):
+        pass
 
 if __name__ == '__main__':
     sp = Sphere((0, 0, 0), 5)
