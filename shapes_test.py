1fcf15d8a26def85734e10cf58e41a0fb9081653
Lahav174 <lahav.lipson@columbia.edu>
2019-03-03 11:54:59 -0500

    Updating shapes_test.py and gitignore.

--- shapes_test.py
+++ shapes_test.py
@@ -1,7 +1,8 @@
 from shapes import Sphere, Cuboid, Tetrahedron
 from renderer import Renderer, Cam, Lit, Tri
+import cv2
 
-rend = Renderer("./Renderer")
+rend = Renderer("")
 
 sp = Sphere((0, 0, 0), 25)
 shapes = sp.render()
@@ -24,5 +25,7 @@ background_prims.append(Tri([(-1000.00,-40.00,-1000.00), (1000.00,-40.00, 1000.0
 
 
 light = Lit((125,300,35),79000)
-camera = Cam((200,222,83),( -.5,-.7,-.5), (640,480))
-shadow_numpy_arr, shadowless_numpy_arr = rend.render(camera, light, shapes, background_prims)
+camera = [Cam((200,222,83),( -.5,-.7,-.5), (640,480))]
+shadow, noshadow = rend.render(camera, light, shapes, background_prims)[0]
+cv2.imwrite("shadow.png", shadow)
+cv2.imwrite("noshadow.png", noshadow)
