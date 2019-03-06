92e165239ddb3ac8a88e44b67e157224684a56e7
Lahav174 <lahav.lipson@columbia.edu>
2019-03-05 23:30:15 -0500

    Implemented train() method.

--- experiment.py
+++ experiment.py
@@ -17,7 +17,12 @@ class Experiment:
         self.network = ShadowNet()
         self.training_losses = []
         self.EPOCHS = args.niter
-
+        self.cuda = args.cuda
+        self.pixelwise_loss = torch.nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.network.parameters(), lr=args.lr)
+        if self.cuda:
+            self.network = self.network.cuda()
+            self.pixelwise_loss = self.pixelwise_loss.cuda()
 
     def run(self):
 
@@ -26,15 +31,31 @@ class Experiment:
 
     def train(self, epoch):
         print("Training Epoch",epoch)
-        print(len(self.dataset))
+        self.network.train()
+        running_loss = []
         for i, (shadowless_views, shadowed_views) in enumerate(self.dataloader):
-            print("Iteration",i, shadowless_views[0].squeeze(0).shape, shadowless_views.min(), shadowless_views.max())
-            ShapeDataset.print_tensor(shadowless_views[0], "shadowless_views.png")
-            #estimated_shadowed_views = self.network(shadowless_views)
+            if self.cuda:
+                shadowless_views = shadowless_views.cuda()
+                shadowed_views = shadowed_views.cuda()
+            self.optimizer.zero_grad()
 
+            estimated_shadowed_views = self.network(shadowless_views)
+            training_loss = self.pixelwise_loss(estimated_shadowed_views, shadowed_views)
+            running_loss.append(training_loss.item())
+            print("Training loss:",mean(running_loss))
+            training_loss.backward()
+            self.optimizer.step()
 
-    def evaluate(self, num_samples):
-        pass
+        self.training_losses.append(mean(running_loss))
+
+            # print("Iteration",i, shadowless_views[0].squeeze(0).shape, shadowless_views.min(), shadowless_views.max())
+            # ShapeDataset.print_tensor(shadowless_views[0], "shadowless_views.png")
+
+            # print("Finished iteration")
+
+    # def evaluate(self, num_samples):
+    #     for i, (shadowless_views, shadowed_views) in enumerate(self.dataloader):
+
 
 
 
